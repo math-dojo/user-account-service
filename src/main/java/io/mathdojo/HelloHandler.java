5fd4a6c2a88f0d90116fc51f2dd94e541611eb7f
Nsikan Essien <nsikane@live.co.uk>
2020-03-23 23:34:50 +0000

    Linting fixes

--- src/main/java/io/mathdojo/HelloHandler.java
+++ src/main/java/io/mathdojo/HelloHandler.java
@@ -15,8 +15,8 @@ import java.util.Optional;
 public class HelloHandler extends AzureSpringBootRequestHandler<User, Greeting> {
 
     @FunctionName("hello")
-    public Greeting execute(
-            @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+    public Greeting execute(@HttpTrigger(name = "request", methods = { HttpMethod.GET,
+            HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
             ExecutionContext context) {
 
         context.getLogger().info("Greeting user name: " + request.getBody().get().getName());
