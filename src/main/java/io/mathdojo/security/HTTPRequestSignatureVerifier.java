8b54669027acc8a64a748f61389df11598e5ff59
Nsikan Essien <nsikane@live.co.uk>
2020-03-28 12:30:54 +0000

    Add method and test for extracting signature value

--- src/main/java/io/mathdojo/security/HTTPRequestSignatureVerifier.java
+++ src/main/java/io/mathdojo/security/HTTPRequestSignatureVerifier.java
@@ -1,5 +1,7 @@
 package io.mathdojo.security;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HTTPRequestSignatureVerifier {
@@ -13,6 +15,21 @@ public class HTTPRequestSignatureVerifier {
 			return false;
 		}
 		return true;
+	}
+
+	public String extractSignatureStringFromSignatureHeader(String signatureHeaderValue) {
+		String headerValueWithoutSignaturePrefix = signatureHeaderValue.replaceAll("Signature ", "");
+		String[] listOfSignatureValues = headerValueWithoutSignaturePrefix.split(",");
+		Map<String, String> signatureValueContents =  new HashMap<>();
+
+		Arrays.stream(listOfSignatureValues).forEach(each -> {
+			String[] arrayOfSplitContents = each.split("=");
+			String signatureFieldName = arrayOfSplitContents[0];
+			String signatureValueWithQuotes = arrayOfSplitContents[1].replace("\"", "");
+			signatureValueContents.put(signatureFieldName, signatureValueWithQuotes);
+		});
+
+		return signatureValueContents.get("signature");
 	}
 
 }
--- src/test/java/io/mathdojo/security/HTTPRequestSignatureVerifierTest.java
+++ src/test/java/io/mathdojo/security/HTTPRequestSignatureVerifierTest.java
@@ -1,15 +1,19 @@
 package io.mathdojo.security;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
 public class HTTPRequestSignatureVerifierTest {
-    
+
     @Test
     public void testReturnsFalseIfNoSignatureHeader() {
         Map<String, String> testHeaders = new HashMap<String, String>();
@@ -18,7 +22,7 @@ public class HTTPRequestSignatureVerifierTest {
         HTTPRequestSignatureVerifier verifier = new HTTPRequestSignatureVerifier();
         assertFalse(verifier.verifySignatureHeader(testHeaders));
     }
-    
+
     @Test
     public void testReturnsTrueIfSignatureHeader() {
         Map<String, String> testHeaders = new HashMap<String, String>();
@@ -27,5 +31,25 @@ public class HTTPRequestSignatureVerifierTest {
         HTTPRequestSignatureVerifier verifier = new HTTPRequestSignatureVerifier();
         assertTrue(verifier.verifySignatureHeader(testHeaders));
     }
-    
+
+    @Test
+    public void testExtractsSignatureStringFromSignatureHeader() {
+        List<String> testHeaderList = Arrays.asList("header1", "header2");
+        String testSignatureString = "somethingSignedAndB64Encoded";
+        String testSignatureHeaderValue = createSignatureString("someKeyId", "someAlg", testHeaderList,
+        testSignatureString);
+
+        HTTPRequestSignatureVerifier verifier = new HTTPRequestSignatureVerifier();
+
+        String extractedSignatureString = verifier.extractSignatureStringFromSignatureHeader(testSignatureHeaderValue);
+        assertEquals(testSignatureString, extractedSignatureString);
+    }
+
+    private String createSignatureString(String keyId, String algorithm, List<String> headersInSignature,
+            String signatureString) {
+        String spaceSeparatedHeaderNames = headersInSignature.stream().reduce("",
+                (accumulatedString, currentValue) -> accumulatedString + " " + currentValue);
+        return String.format("Signature keyId=\"%s\",algorithm=\"%s\",headers=\"%s\",signature=\"%s\"", keyId,
+                algorithm, spaceSeparatedHeaderNames, signatureString);
+    }
 }
