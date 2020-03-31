6ac4d051c293caa534ffddf7226d1b4f9efe8296
Nsikan Essien <nsikane@live.co.uk>
2020-03-31 22:00:15 +0100

    Integrate request signatures in handler

--- src/main/java/io/mathdojo/HelloHandler.java
+++ src/main/java/io/mathdojo/HelloHandler.java
@@ -1,30 +1,74 @@
 package io.mathdojo;
 
-import io.mathdojo.model.Greeting;
-import io.mathdojo.model.User;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Level;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
-import java.util.Optional;
+import io.mathdojo.model.Greeting;
+import io.mathdojo.model.User;
+import io.mathdojo.security.HTTPRequestSignatureVerificationException;
+import io.mathdojo.security.HTTPRequestSignatureVerifier;
 
 public class HelloHandler extends AzureSpringBootRequestHandler<User, Greeting> {
 
     @FunctionName("hello")
-    public Greeting execute(@HttpTrigger(name = "request", methods = { HttpMethod.GET,
+    public HttpResponseMessage execute(@HttpTrigger(name = "request", methods = { HttpMethod.GET,
             HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
-            ExecutionContext context) {
+            ExecutionContext context) throws NoSuchAlgorithmException {
 
+        if (!System.getenv("MATH_DOJO_ENV_NAME").equals("local")) {
+            HTTPRequestSignatureVerifier verifier = createVerifier(
+                    System.getenv("MATH_DOJO_HTTP_REQUEST_SIGNATURE_EXPECTED_KEYID"),
+                    System.getenv("MATH_DOJO_HTTP_REQUEST_SIGNATURE_B64_DER_PUBLIC_KEY"));
+            try {
+                boolean verificationResult = verifier.verifySignatureHeader(request.getHeaders(),
+                        request.getUri().getPath(), request.getHttpMethod());
+                if(!verificationResult) {
+                    return request.createResponseBuilder(HttpStatus.UNAUTHORIZED)
+                        .body("signature verification failed")
+                        .build();                    
+                }
+            } catch (InvalidKeyException | SignatureException | UnsupportedEncodingException
+                    | HTTPRequestSignatureVerificationException e) {
+                    context.getLogger().log(
+                        Level.WARNING, "signature verification threw an exception", e);
+                    return request.createResponseBuilder(HttpStatus.UNAUTHORIZED)
+                        .body("signature verification failed")
+                        .build();
+            }
+        }
+
+        context.getLogger().info("URI path is: "+request.getUri().getPath());
         context.getLogger().info("Got headers: ");
         request.getHeaders().forEach((eachKey, eachValue) -> context.getLogger()
                 .info("Got header " + eachKey + " with value: " + eachValue));
         context.getLogger().info("Greeting user name: " + request.getBody().get().getName());
-        return handleRequest(request.getBody().get(), context);
+
+        return request.createResponseBuilder(HttpStatus.OK)
+                .body(handleRequest(request.getBody().get(), context))
+                .build();
+    }
+
+    private HTTPRequestSignatureVerifier createVerifier(String expectedKeyId, String b64EncDerOfPublicKey)
+            throws NoSuchAlgorithmException {
+        Map<String, String> mapOfKeyIdAndB64EncDerPubKey = Collections.singletonMap(expectedKeyId,
+                b64EncDerOfPublicKey);
+        return new HTTPRequestSignatureVerifier(mapOfKeyIdAndB64EncDerPubKey);
     }
 }
