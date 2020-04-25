ffb72b8289d43377b2f05fad3403f13b1e55f340
Nsikan Essien <nsikane@live.co.uk>
2020-04-26 00:22:09 +0100

    Fix failing integration tests

--- integration-tests/features/step_definitions/response_steps.js
+++ integration-tests/features/step_definitions/response_steps.js
@@ -10,14 +10,15 @@ const { payloads } = require('../support/payloads');
 
 Then(/I should get a status code (\d{3})/, function (expectedCode) {
     return Promise.all([
-        expect(this.world.response).to.eventually.to.have.deep.own.
+        expect(this.world.response).to.eventually.have.deep.own.
             property('status', Number.parseInt(expectedCode))
     ]);
 });
 
 Then(/the response should contain all the keys and values set from \'(\w+)\'/, function (nameOfSchemaOfExpectedContents) {
     return Promise.all([
-        expect(this.world.response).to.eventually.to.have.deep
-            .include({'data': payloads[nameOfSchemaOfExpectedContents]})
+        expect(this.world.response).to.eventually.satisfy(function(response) {
+            return expect(response.data).to.deep.include(payloads[nameOfSchemaOfExpectedContents]);
+          })
     ]);
 });
