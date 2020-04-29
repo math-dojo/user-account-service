ccffb72b221f9f2dc1ace316c32b99ac023d9eae
Nsikan Essien <nsikane@live.co.uk>
2020-04-29 15:47:00 +0100

    Add handler for getOrg by Id

---
+++ src/main/java/io/mathdojo/useraccountservice/BodyLessOrganisationsRequestHandler.java
@@ -0,0 +1,47 @@
+package io.mathdojo.useraccountservice;
+
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import io.mathdojo.useraccountservice.model.Organisation;
+import io.mathdojo.useraccountservice.security.HTTPRequestSignatureVerificationEnabledHandler;
+import io.mathdojo.useraccountservice.services.OrganisationServiceException;
+
+public class BodyLessOrganisationsRequestHandler
+        extends HTTPRequestSignatureVerificationEnabledHandler<String, Organisation> {
+
+    @FunctionName("getOrganisationById")
+    public HttpResponseMessage executeGetByIdForOrganisations(
+        @HttpTrigger(
+            name = "request", 
+            methods = { HttpMethod.GET }, 
+            authLevel = AuthorizationLevel.ANONYMOUS,
+            route = "organisations"
+        ) HttpRequestMessage<Optional<String>> request,
+        ExecutionContext context) {
+
+            try {
+                Organisation createdOrg = (Organisation) handleRequest(request, request.getBody().get(), context);
+                return request.createResponseBuilder(HttpStatus.CREATED)
+                    .body(createdOrg)
+                    .build();
+
+            } catch (OrganisationServiceException e) {
+                return request.createResponseBuilder(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage())
+                    .build();
+            } catch (Exception e) {
+                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+            }
+
+    }
+}
