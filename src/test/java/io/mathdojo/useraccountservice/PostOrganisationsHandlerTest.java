9b1b4885e1675c0d8ec244410db699c81832d63e
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-05-02 17:36:59 +0100

    Check post handler responds with 401 on verif error

---
+++ src/test/java/io/mathdojo/useraccountservice/PostOrganisationsHandlerTest.java
@@ -0,0 +1,57 @@
+package io.mathdojo.useraccountservice;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import io.mathdojo.useraccountservice.model.requestobjects.AccountRequest;
+
+public class PostOrganisationsHandlerTest {
+    private ExecutionContext mockExecContext;
+
+    @Before
+    public void setup() {
+        Logger testLogger = mock(Logger.class);
+        mockExecContext = mock(ExecutionContext.class);
+
+        Mockito.when(mockExecContext.getLogger()).thenReturn(testLogger);
+    }
+
+    @Test
+    public void testGetOrgByIdReturns401FromVerificationFailure() {
+        PostOrganisationsHandler handler = new PostOrganisationsHandler();
+        PostOrganisationsHandler handlerSpy = Mockito.spy(handler);
+
+        HttpRequestMessage<Optional<AccountRequest>> mockMessage = (HttpRequestMessage<Optional<AccountRequest>>) mock(HttpRequestMessage.class);
+        AccountRequest mockAccountRequest = mock(AccountRequest.class);
+        Optional<AccountRequest> mockAccountRequestOptional = Optional.of(mockAccountRequest);
+
+        when(mockMessage.getBody()).thenReturn(mockAccountRequestOptional);
+
+        HttpResponseMessage expectedResponseFromSignatureVerifier = mock(HttpResponseMessage.class);
+        when(expectedResponseFromSignatureVerifier.getStatus()).thenReturn(HttpStatus.UNAUTHORIZED);
+
+        doReturn(expectedResponseFromSignatureVerifier).when(handlerSpy).handleRequest(
+            any(HttpRequestMessage.class), any(AccountRequest.class), any(ExecutionContext.class));
+
+        HttpResponseMessage actualResponseMessage = handlerSpy.executePostForOrganisations(
+            mockMessage, mockExecContext);
+
+        assertEquals(expectedResponseFromSignatureVerifier.getStatus(), actualResponseMessage.getStatus());
+
+    }    
+}
