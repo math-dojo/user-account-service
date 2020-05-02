3333f0d7008ce1940b1f8670c047da5b908d731b
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-05-02 11:56:00 +0100

    Add unit test to cover 401 responses from verifier

---
+++ src/test/java/io/mathdojo/useraccountservice/BodyLessOrganisationsRequestHandlerTest.java
@@ -0,0 +1,51 @@
+package io.mathdojo.useraccountservice;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.logging.Logger;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class BodyLessOrganisationsRequestHandlerTest {
+    private ExecutionContext mockExecContext;
+    private HttpRequestMessage mockMessage;
+
+    @Before
+    public void setup() {
+        Logger testLogger = mock(Logger.class);
+        mockExecContext = mock(ExecutionContext.class);
+        mockMessage = mock(HttpRequestMessage.class);
+
+        Mockito.when(mockExecContext.getLogger()).thenReturn(testLogger);
+    }
+
+    @Test
+    public void testGetOrgByIdReturns401FromVerificationFailure() {
+        BodyLessOrganisationsRequestHandler handler = new BodyLessOrganisationsRequestHandler();
+        BodyLessOrganisationsRequestHandler handlerSpy = Mockito.spy(handler);
+
+        HttpResponseMessage expectedResponseFromSignatureVerifier = mock(HttpResponseMessage.class);
+        when(expectedResponseFromSignatureVerifier.getStatus()).thenReturn(HttpStatus.UNAUTHORIZED);
+
+        doReturn(expectedResponseFromSignatureVerifier).when(handlerSpy).handleRequest(
+            any(HttpRequestMessage.class), anyString(), any(ExecutionContext.class));
+
+        HttpResponseMessage actualResponseMessage = handlerSpy.executeGetByIdForOrganisations(
+            mockMessage, "orgId", mockExecContext);
+
+        assertEquals(expectedResponseFromSignatureVerifier.getStatus(), actualResponseMessage.getStatus());
+
+    }
+}
