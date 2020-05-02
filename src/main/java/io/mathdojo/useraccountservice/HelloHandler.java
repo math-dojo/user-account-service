7e0601162a2798cb18c5c1ba1b37f9f2833ea110
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-05-02 05:33:25 +0100

    Add error handling to hello function

--- src/main/java/io/mathdojo/useraccountservice/HelloHandler.java
+++ src/main/java/io/mathdojo/useraccountservice/HelloHandler.java
@@ -1,6 +1,7 @@
 package io.mathdojo.useraccountservice;
 
 import java.util.Optional;
+import java.util.logging.Level;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
@@ -22,14 +23,19 @@ public class HelloHandler extends HTTPRequestSignatureVerificationEnabledHandler
             HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<DummyUser>> request,
             ExecutionContext context) {
 
-        context.getLogger().info("URI path is: "+request.getUri().getPath());
+                context.getLogger().info("URI path is: " + request.getUri().getPath());
         context.getLogger().info("Got headers: ");
         request.getHeaders().forEach((eachKey, eachValue) -> context.getLogger()
                 .info("Got header " + eachKey + " with value: " + eachValue));
         context.getLogger().info("Greeting user name: " + request.getBody().get().getName());
 
+                try {
         return request.createResponseBuilder(HttpStatus.OK)
-                .body(handleRequest(request, request.getBody().get(), context))
-                .build();
+                                .body(handleRequest(request, request.getBody().get(), context)).build();
+
+                } catch (Exception e) {
+                        context.getLogger().log(Level.WARNING, "Hello handler failed", e);
+                        return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                }
     }
 }
